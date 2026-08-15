ee81398262083b05f549e403b3f5b20390398dec
kulpati <kulpatikumawat@gmail.com>
2026-08-15 23:07:20 +0530

    linked List

---
+++ problems/LinkedList/Copy_List_With_Random_Pointer.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+import org.w3c.dom.Node;
+
+public class Copy_List_With_Random_Pointer{
+    class Solution {
+    public Node copyRandomList(Node head) {
+        if(head==null){return head;}
+        HashMap<Node,Node> map=new HashMap<>();
+        Node newhead=new Node(head.val);
+        map.put(head,newhead);
+        Node temp=head.next;
+        Node tp=newhead;
+        while(temp!=null){
+            tp.next=new Node(temp.val);
+            tp=tp.next;
+            map.put(temp,tp);
+            temp=temp.next;
+        }
+        tp.next=null;
+        Node tm=head;
+        Node dm=newhead;
+        while(tm!=null){
+            dm.random=map.get(tm.random);
+            dm=dm.next;
+            tm=tm.next;
+        }
+    return newhead;}
+}
+}
