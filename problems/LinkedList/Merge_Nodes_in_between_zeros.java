c82bb89ecc34583a8a19067871ad4a888b683ba7
kulpati <kulpatikumawat@gmail.com>
2026-08-16 23:29:14 +0530

    Solved Linked List

---
+++ problems/LinkedList/Merge_Nodes_in_between_zeros.java
@@ -0,0 +1,29 @@
+public class Merge_Nodes_in_between_zeros {
+    
+ 
+ public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode temp=head.next;
+        ListNode dm=new ListNode();
+        ListNode nn=dm;
+        int sum=0;
+        while(temp!=null){
+            sum+=temp.val;
+            if(temp.val==0){
+                nn.next=new ListNode(sum);
+                sum=temp.val;
+                nn=nn.next;
+            }
+            temp=temp.next;
+        }
+   return dm.next; }
+}
+}
