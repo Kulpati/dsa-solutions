4cddd38bec0d0d5a260e95cb3367c323e1b92f8c
kulpati <kulpatikumawat@gmail.com>
2026-08-23 21:54:08 +0530

    recursion

---
+++ problems/Recursion/Power_Two.java
@@ -0,0 +1,16 @@
+class Solution {
+    private boolean recur(int n){
+        if(n==1){
+            return true;
+        }
+        if(n%2!=0){
+            return false;
+        }
+        
+        return recur(n>>1);
+    }
+    public boolean isPowerOfTwo(int n) {
+        if(n==0){return false;}
+        return recur(n);
+    }
+}
