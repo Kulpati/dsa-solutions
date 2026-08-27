2ed02370f2163f9fdb49c0e3b2cf3bb4aee7b154
kulpati <kulpatikumawat@gmail.com>
2026-08-27 21:26:23 +0530

    Permutations

---
+++ problems/Recursion/Permutation.java
@@ -0,0 +1,30 @@
+ package Recursion;
+
+class Permutation {
+ class Solution {
+    List<List<Integer>> re;
+    private void func(int[] nums,List<Integer> list,boolean[] used){
+        if(list.size()==nums.length){
+            re.add(new ArrayList<>(list));
+            return ;
+        }
+        for(int i=0;i<used.length;i++){
+            if(used[i]){continue;}
+            list.add(nums[i]);
+            used[i]=true;
+            func(nums,list,used);
+            list.remove(list.size()-1);
+            used[i]=false;
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        boolean used[]=new boolean[nums.length];
+        Arrays.fill(used,false);
+        re=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        func(nums,list,used);
+        return re;
+    }
+}
+    
+}
